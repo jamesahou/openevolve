eeb6eac244d3462d253981d1cf3d100cee66d79b
ryanrudes <ryanrudes@gmail.com>
2025-06-08 15:47:06 -0700

    Refactor container path constants for consistency and clarity

--- funsearch/constants.py
+++ funsearch/constants.py
@@ -5,6 +5,11 @@ HOTSWAP_ENVVAR = "FUNSEARCH_HOTSWAP_IMP"
 
 # Absolute paths in the container file system
 WORKSPACE_ROOT: ContainerAbsPath = "/workspace"
-IMPLEMENTATIONS_CONTAINER_PATH: ContainerAbsPath = "/implementations"
+IMPS_CONTAINER_PATH: ContainerAbsPath = "/imps"
+LOGS_CONTAINER_PATH: ContainerAbsPath = "/logs"
 EVAL_CONTAINER_PATH: ContainerAbsPath = "/eval.py"
 MAIN_CONTAINER_PATH: ContainerAbsPath = "/main.py"
+
+# Container constants
+SANDBOX_IMAGE_NAME = "funsearch_image"
+SANDBOX_CONTAINER_NAME = "funsearch_sandbox"
