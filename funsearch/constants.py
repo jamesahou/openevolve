68c317ad3215f6e316044467e8bf3a05e7668d76
ryanrudes <ryanrudes@gmail.com>
2025-06-08 15:36:46 -0700

    Refactor constants in FunSearch to include absolute paths for container file system

--- funsearch/constants.py
+++ funsearch/constants.py
@@ -1,2 +1,10 @@
 """Project constants for FunSearch."""
+from funsearch.custom_types import ContainerAbsPath
+
 HOTSWAP_ENVVAR = "FUNSEARCH_HOTSWAP_IMP"
+
+# Absolute paths in the container file system
+WORKSPACE_ROOT: ContainerAbsPath = "/workspace"
+IMPLEMENTATIONS_CONTAINER_PATH: ContainerAbsPath = "/implementations"
+EVAL_CONTAINER_PATH: ContainerAbsPath = "/eval.py"
+MAIN_CONTAINER_PATH: ContainerAbsPath = "/main.py"
