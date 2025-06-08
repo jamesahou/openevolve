016d36bb236f4499d1ccccbb5a2f865477636796
ryanrudes <ryanrudes@gmail.com>
2025-06-08 16:55:42 -0700

    Refactor constants to use Path for container file system paths

--- openevolve/constants.py
+++ openevolve/constants.py
@@ -1,14 +1,16 @@
 """Project constants for FunSearch."""
 from openevolve.custom_types import ContainerAbsPath
+from pathlib import Path
 
 HOTSWAP_ENVVAR = "FUNSEARCH_HOTSWAP_IMP"
 
 # Absolute paths in the container file system
-WORKSPACE_ROOT: ContainerAbsPath = "/workspace"
-IMPS_CONTAINER_PATH: ContainerAbsPath = "/imps"
-LOGS_CONTAINER_PATH: ContainerAbsPath = "/logs"
-INPUTS_CONTAINER_PATH: ContainerAbsPath = "/inputs"
-EVAL_CONTAINER_PATH: ContainerAbsPath = "/eval.py"
+WORKSPACE_ROOT: ContainerAbsPath = Path("/workspace")
+IMPS_CONTAINER_PATH: ContainerAbsPath = Path("/imps")
+LOGS_CONTAINER_PATH: ContainerAbsPath = Path("/logs")
+INPUTS_CONTAINER_PATH: ContainerAbsPath = Path("/inputs")
+EVAL_CONTAINER_PATH: ContainerAbsPath = WORKSPACE_ROOT / "eval.py"
+SETUP_CONTAINER_PATH: ContainerAbsPath = WORKSPACE_ROOT / "setup.py"
 MAIN_CONTAINER_PATH: ContainerAbsPath = "/main.py"
 CONTAINER_PYTHONPATH: ContainerAbsPath = "/usr/local/bin/python3"
 
