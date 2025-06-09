edb1a9d884d1e6ccf7c1abbc2ff02748f690b142
ryanrudes <ryanrudes@gmail.com>
2025-06-09 13:48:45 -0700

    Add more tests

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -1,6 +1,7 @@
 from openevolve.sandbox import ContainerSandbox, ContainerEngine, DummySandbox
 from openevolve.constants import SANDBOX_IMAGE_NAME, SANDBOX_CONTAINER_NAME
 from openevolve.custom_types import HostAbsPath, HostRelPath
+from openevolve.test_case import TestCase
 
 from pathlib import Path
 
@@ -18,7 +19,7 @@ def reset_sandbox_id():
     yield
     DummySandbox.sandboxes = 0
 
-def test_builds_image():
+def test_build_image():
     # Get the path to the project root
     project_root: HostAbsPath = Path("/Users/ryanrudes/GitHub/OpenEvolve/astropy")
 
@@ -34,11 +35,27 @@ def test_builds_image():
         setup_relpath=setup_path,
     )
 
-def test_creates_sandbox():
-    # Create temporary directory to store implementations
+def test_create_sandbox():
     imps_root: HostAbsPath = Path("/Users/ryanrudes/GitHub/OpenEvolve/openevolve/imps")
 
     ContainerSandbox.create_container(imps_root)
 
-def test_starts_sandbox():
+def test_start_sandbox():
     ContainerSandbox.start_container()
+
+def test_upload_test_cases():
+    dummy_test_cases = [
+        TestCase(
+            args = [1, 2],
+            kwargs= {}
+        ),
+        TestCase(
+            args = [3, 4],
+            kwargs= {}
+        ),
+    ]
+
+    ContainerSandbox.upload_test_cases(dummy_test_cases)
+
+def test_remove_container():
+    ContainerSandbox.remove_container()
